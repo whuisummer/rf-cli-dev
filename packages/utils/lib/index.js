b3b026422847996aa79f55906a00a9b59df62583
王辉 <hwang090@mail.nfsq.com.cn>
2021-12-30 13:16:15 +0800

    fix(test):

--- packages/utils/lib/index.js
+++ packages/utils/lib/index.js
@@ -1,7 +1,16 @@
-'use strict';
+/*
+ * @Description:
+ * @Version: 2.0
+ * @Autor: hwang
+ * @Date: 2021-12-26 18:35:29
+ * @LastEditors: hwang
+ * @LastEditTime: 2021-12-30 13:12:56
+ */
+"use strict";
 
 module.exports = utils;
 
 function utils() {
-    // TODO
+  // TODO
+  console.log("test");
 }
